363776b7a521f0f8284eac005ba8914d0f9013b7
rigole <foplacide@gmail.com>
2023-10-06 09:36:12 +0100

    fixing memory leaks and somme css problems

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -8,16 +8,16 @@ import { ModalService } from 'src/app/services/modal.service';
   //providers: [ModalService]
 })
 export class ModalComponent implements OnInit {
-
-  constructor(public modal: ModalService){
-
+  @Input() modalID = ''
+  constructor(public modal: ModalService, public el: ElementRef){
+      console.log(el);
   }
 
   ngOnInit(): void {
-    
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal(){
-    this.modal.toggleModal()
+    this.modal.toggleModal(this.modalID)
   }
 }

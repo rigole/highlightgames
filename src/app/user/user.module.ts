22d63a5e72b88cd70dc2e9fd7c3866f627d8fbdb
rigole <foplacide@gmail.com>
2023-10-08 20:59:50 +0100

    binding form and getting the input value of the name field

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ], 
   exports: [
     AuthModalComponent

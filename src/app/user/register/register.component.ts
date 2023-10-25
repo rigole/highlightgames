bec8db8ee2e3c1531b7f7ba58e3f4b3bc6e759f4
rigole <foplacide@gmail.com>
2023-10-25 17:04:50 +0100

    saving users by using a service

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from 'src/app/services/auth.service';
+import IUser from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent {
 
   constructor(
-    private auth: AngularFireAuth,
-    private db: AngularFirestore
-    )
-  {}
+    private auth: AuthService
+  ){}
   
   showAlert = false
   alertMsg = 'Please wait! Your account is being created.'
@@ -26,22 +25,27 @@ export class RegisterComponent {
     Validators.required,
     Validators.minLength(3)
    ])
+
    email = new FormControl('', [
       Validators.required,
       Validators.email
    ])
-   age = new FormControl('', [
+
+   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
     Validators.max(90)
    ])
+
    password = new FormControl('', [
     Validators.required,
     Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$/gm)
    ])
+
    confirm_password = new FormControl('', [
     Validators.required
    ])
+
    phoneNumber = new FormControl('', [
       Validators.required,
       Validators.minLength(9),
@@ -63,20 +67,10 @@ export class RegisterComponent {
     this.alertColor = 'blue'
     this.inSubmission = true
 
-    const { email, password } =  this.registerForm.value
+    
 
     try {
-      const userCredentials = await this.auth.createUserWithEmailAndPassword(
-        email as string , password as string
-      )
-
-      await this.db.collection('users').add({
-        name : this.name.value,
-        email: this.email.value,
-        age: this.age.value ,
-        phoneNumber: this.phoneNumber.value 
-      })
-      //console.log(userCredentials)
+     await this.auth.createUser(this.registerForm.value as IUser)
     } catch (error) {
        console.error(error)
        this.alertMsg = 'An unexepected error occured!'

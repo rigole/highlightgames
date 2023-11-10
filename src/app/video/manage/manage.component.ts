31c1f6de49cbb9a28a1787439b4cadd8319b190c
rigole <foplacide@gmail.com>
2023-11-10 13:07:44 +0100

    adding query parameter for helping sort video

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component , OnInit} from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 
 @Component({
@@ -9,13 +10,23 @@ import { Component , OnInit} from '@angular/core';
 
 
 export class ManageComponent implements OnInit{
+  videoOrder = '1'
 
-  constructor(){
-
-  }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+    ){}
 
   ngOnInit(): void {
-     
+     this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params['sort'] === '2' ? params['sort'] : '1'
+     })
+  }
+
+  sort(event: Event){
+    const { value } = (event.target as HTMLSelectElement)
+
+    this.router.navigateByUrl(`/manage?sort=${value}`)
   }
 
 }

6c93a4faf8d45eef6ae1f2634c7bab10186c0bdc
rigole <foplacide@gmail.com>
2023-11-14 13:04:50 +0100

    handling the drag and drop event and handling the file

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent {
+export class UploadComponent implements OnInit{
+  isDragover = false
+  file: File | null = null
 
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  storeFile($event: Event){
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    this.isDragover = false
+
+    if(!this.file || this.file.type !== 'video/mp4'){
+        return
+    }
+    console.log(this.file)
+  }
 }

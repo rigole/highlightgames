b7260bbc6319c30cfad26647a383213fe4a15162
rigole <foplacide@gmail.com>
2024-01-06 16:11:21 +0100

    adding the editing form + preparing the modal for editing

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component , OnInit} from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
 
 
 @Component({
@@ -18,7 +19,8 @@ export class ManageComponent implements OnInit{
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modal: ModalService
     ){}
 
   ngOnInit(): void {
@@ -47,6 +49,12 @@ export class ManageComponent implements OnInit{
         sort: value
       }
     })
+  }
+
+  openModal($event: Event, clip: IClip){
+    $event.preventDefault()
+
+    this.modal.toggleModal('editClip')
   }
 
 }
